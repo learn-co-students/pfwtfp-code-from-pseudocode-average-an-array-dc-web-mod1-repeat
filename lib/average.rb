79ee079f4c3a2e663f586350959228343a32a9d8
Michael Sackstein <m_sackstein@yahoo.com>
2020-04-14 22:06:34 -0400

    Done.

--- lib/average.rb
+++ lib/average.rb
@@ -1,1 +1,15 @@
-# Add your code, translated from pseudocode, below!
+def average_numbers(collection)
+    collection_count = collection.count
+    running_total = 0.0
+    0.upto(collection.count - 1) do |i|
+        running_total += collection[i]
+    end
+    if running_total == 0.0
+        average = 0.00
+    else
+        average = running_total / collection_count
+        average.round(2)
+    end
+end
+# collection = [1,7,3]
+# puts average_numbers(collection)
